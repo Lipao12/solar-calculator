6bc314745ed71dedfbdc13670d40cd0687956fd8
Filipe Mai <f.nunesmai19@gmail.com>
2024-09-17 19:21:09 -0300

    feat: creation dataset panels and inverters

---
+++ src/assets/banco_inversor.ts
@@ -0,0 +1,23 @@
+export const inverters = [
+    { indice: 1, preco: 7234.47, potencia: 3000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.979 },
+    { indice: 2, preco: 7829.47, potencia: 4000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.980 },
+    { indice: 3, preco: 11893.47, potencia: 8200, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.981 },
+    { indice: 4, preco: 4928.07, potencia: 4000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.974 },
+    { indice: 5, preco: 1421.00, potencia: 1000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.900 },
+    { indice: 6, preco: 6304.47, potencia: 5700, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.980 },
+    { indice: 7, preco: 17324.97, potencia: 15000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.977 },
+    { indice: 8, preco: 10303.47, potencia: 6000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.978 },
+    { indice: 9, preco: 20059.17, potencia: 25000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.982 },
+    { indice: 10, preco: 21370.47, potencia: 27000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.983 },
+    { indice: 11, preco: 3644.47, potencia: 1200, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.965 },
+    { indice: 12, preco: 15464.97, potencia: 15000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.970 },
+    { indice: 13, preco: 18673.97, potencia: 20000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.970 },
+    { indice: 14, preco: 24932.97, potencia: 30000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.970 },
+    { indice: 15, preco: 17464.97, potencia: 12000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.970 },
+    { indice: 16, preco: 21017.07, potencia: 27000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.983 },
+    { indice: 17, preco: 4704.87, potencia: 5000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.981 },
+    { indice: 18, preco: 3440.07, potencia: 3000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.977 },
+    { indice: 19, preco: 4928.07, potencia: 3000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.977 },
+    { indice: 20, preco: 5114.97, potencia: 5000, quantosInversores: 0, precoTotal: 0, rendimentoMaximoInversor: 0.980 }
+  ];
+  
---
+++ src/assets/banco_paineis.ts
@@ -0,0 +1,93 @@
+export const solarPanels = [
+    {
+      nome: "Painel Solar 305W Monocristalino Canadian Solar - CS6K-305M",
+      potencia: 305,
+      preco: 631.48,
+      area: 1.6368,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.39,
+      noct: 45
+    },
+    {
+      nome: "Painel Solar 285W Canadian Solar Policristalino - CS6K-285P",
+      potencia: 285,
+      preco: 491.97,
+      area: 1.6368,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.39,
+      noct: 43
+    },
+    {
+      nome: "Painel Solar Fotovoltaico 155W - Upsolar UP-M155P",
+      potencia: 155,
+      preco: 329.00,
+      area: 0.99026,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.43,
+      noct: 45
+    },
+    {
+      nome: "Painel Solar Fotovoltaico 335W - Upsolar UP-M335P",
+      potencia: 335,
+      preco: 579.00,
+      area: 1.93936,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.4,
+      noct: 45
+    },
+    {
+      nome: "Painel Solar Fotovoltaico 280W - Sinosola SA280-60P",
+      potencia: 280,
+      preco: 519.00,
+      area: 1.62688,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.42,
+      noct: 45
+    },
+    {
+      nome: "Painel Solar Fotovoltaico 285W - Upsolar UP-M285P",
+      potencia: 285,
+      preco: 479.00,
+      area: 1.62688,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.42,
+      noct: 45
+    },
+    {
+      nome: "Painel Solar 330W Dah Solar Policristalino - DHP72-330W",
+      potencia: 330,
+      preco: 538.47,
+      area: 1.938396,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.41,
+      noct: 45
+    },
+    {
+      nome: "Painel Solar 405W Canadian Solar - Placa Solar Half Cell",
+      potencia: 405,
+      preco: 780.47,
+      area: 2.209184,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.37,
+      noct: 42
+    },
+    {
+      nome: "Painel Solar Fotovoltaico 380W Monocristalino - DAH Sola",
+      potencia: 380,
+      preco: 696.57,
+      area: 1.940352,
+      qtd: null,
+      custoTotal: null,
+      coefTempPot: -0.37,
+      noct: 45
+    }
+  ];
+  
